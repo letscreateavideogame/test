b8f11030357a8471455b978cc51fffecfa434e62
letscreateavideogame <lets.create.a.video.game@gmail.com>
2018-11-10 01:30:17 +0100

    Cree una caja y la puse guay

---
+++ Assets/script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class script : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        rb.AddForce(0, 0, 300 * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
